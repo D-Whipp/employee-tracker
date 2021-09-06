fe305fe142c8f28267ef4e9d2ec26459a3fa544b
Mr Whipp <dwhipp88@gmail.com>
2021-09-06 02:04:15 -0600

    added tables employee, roles, and department. sourced and created databases created a simple db query using mysql2 documentation

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -6,4 +10,15 @@ CREATE TABLE employees (
     department VARCHAR(30) NOT NULL,
     salary INTEGER NOT NULL,
     manager VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title_name VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+);
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
 )
--- server.js
+++ server.js
@@ -2,17 +2,30 @@ const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
 
-// const db = mysql.createConnection(
-//   {
-//     host: "localhost",
-//     user: "root",
-//     password: "3m!ly!$HighM@int3nance!",
-//     database: "employeeTracker",
-//   },
-//   console.log("Connected to the election database.")
-// );
+initQuestions();
+// create the connection to the database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "3m!ly!$HighM@int3nance!",
+    database: "employeeTracker",
+  },
+  console.log("Connected to the employeeTracker database.")
+);
 
-function initQuestions() {
+// simple query
+// db.query("SELECT * FROM employees", (err, rows) => {
+//   if (err) {
+//     console.log("error: ", err.message);
+//     return;
+//   } else {
+//     console.table("hi");
+//     // initQuestions();
+//   }
+// });
+
+async function initQuestions() {
   inquirer
     .prompt([
       {
@@ -32,7 +45,24 @@ function initQuestions() {
     ])
     .then((answer) => {
       console.log(answer.firstChoice);
+      if (answer.firstChoice === "View All Departments") {
+        console.log("You chose: View All Departments", answer.firstChoice);
+        mysql = "SELECT * FROM employees;";
+        console.table(mysql);
+      } else if (answer.firstChoice === "View All Roles") {
+        console.log("You chose: View All Roles!", answer.firstChoice);
+      } else if (answer.firstChoice === "View All Employees") {
+        console.log("You chose: View All Employees", answer.firstChoice);
+      } else if (answer.firstChoice === "Add A Department") {
+        console.log("You chose Add A Department!", answer.firstChoice);
+      } else if (answer.firstChoice === "Add A Role") {
+        console.log("You chose Add A Role: ", answer.firstChoice);
+      } else if (answer.firstChoice === "Add An Employee") {
+        console.log("You chose Add An Employee: ", answer.firstChoice);
+      } else if (answer.firstChoice === "Update An Employee Role") {
+        console.log("You chose Update An Employee Role: ", answer.firstChoice);
+      }
     });
 }
 
-initQuestions();
+// initQuestions();
